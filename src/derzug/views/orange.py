92f4678a874b32ccfe321c1d15e98bae2faf7bbe
Derrick Chambers <chambers.ja.derrick@gmail.com>
2026-03-23 12:05:05 +0100

    make CI less brittle

--- src/derzug/views/orange.py
+++ src/derzug/views/orange.py
@@ -21,7 +21,6 @@ from pathlib import Path
 from typing import ClassVar
 from xml.sax.saxutils import escape
 
-import derzug._anyqt_patch  # noqa: F401 - imported for side effects before AnyQt.QtGui
 from AnyQt.QtCore import QDir, QEvent, QObject, QPointF, Qt, QTimer, QUrl
 from AnyQt.QtGui import (
     QBrush,
@@ -71,6 +70,8 @@ from orangewidget.workflow.widgetsscheme import (
     WidgetsScheme,
     WidgetsSignalManager,
 )
+
+import derzug._anyqt_patch  # noqa: F401 - imported for side effects before AnyQt.QtGui
 from derzug.annotations_config import (
     AnnotationSettingsDialog,
     load_annotation_config,
